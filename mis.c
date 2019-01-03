33e7f7a8185a719e7a05e41034b4c836008b7f97
Matthias Beaupère <matthias.beaupere@gmail.com>
2019-01-03 13:04:26 +0100

    [Gram-Schmidt] make normalisation and orth. independent

--- mis.c
+++ mis.c
@@ -12,7 +12,7 @@
 double scalar_product(double u[N], double v[N]) {
 	double res = 0;
 
-	for ( int i; i<N; i++) {
+	for ( int i = 0; i<N; i++) {
 		res += u[i]*v[i];
 	}
 
@@ -25,14 +25,14 @@ void orthonormalize(double A[M][N]) {
 	double norm, temp_norm;
 
 	for(int i = 0; i<M; i++) {
-		printf("Traitement du vecteur numéro %d\n", i);
+//		printf("Traitement du vecteur numéro %d\n", i);
 		for (int j = 0; j<N; j++) {
 			temp[j] = A[i][j];
 		}
 
 		for (int k = 0; k<i; k++) {
 
-			printf("calcul de la project sur l'axe %d\n", k);
+//			printf("calcul de la project sur l'axe %d\n", k);
 
 			norm = scalar_product(A[i], A[k]);
 			printf("(u,v) = %f\n", norm);
@@ -43,9 +43,13 @@ void orthonormalize(double A[M][N]) {
 			for (int j = 0; j<N; j++) {
 				temp[j] -= norm * A[i][j];
 			}
-
 		}
+		for (int j = 0; j<N; j++) {
+			A[i][j] = temp[j];
+		}
+	}
 
+	for(int i = 0; i<M; i++) {
 		temp_norm = 0;
 		for ( int k = 0; k<N; k++) {
 			temp_norm += temp[k] * temp[k];
@@ -54,9 +58,11 @@ void orthonormalize(double A[M][N]) {
 		temp_norm = sqrt(temp_norm);
 
 		for (int j = 0; j<N; j++) {
-			A[i][j] = temp[j] / temp_norm;
+			A[i][j] /= temp_norm;
 		}
 	}
+
+
 }
 
 int main() {
@@ -89,7 +95,16 @@ int main() {
 				v[k][i] = v[k][i];
 			}
 		}
+		// q = v
+                for(int i = 0; i<M; i++) {
+			for(int j = 0; j<N; j++) {
+				printf("[%f]",v[i][j]);
+			}
+			printf("\n");
+                }
+                printf("\n");
 
+		printf("Gram-Schmidt ...\n");
 		orthonormalize(v);
 
 		// q = v
