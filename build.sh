fe31e08baf49430ae307eeab69e0409f9ceb9110
Matthias Beaupère <matthias.beaupere@gmail.com>
2019-01-03 12:38:46 +0100

    Fix many errors in orthonormalisation
    + remove old normalisation using alpha

---
+++ build.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+gcc -Wall mis.c -o mis -lm
--- mis.c
+++ mis.c
@@ -4,27 +4,29 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-double scalar_product(double u[3], double v[3]) {
+#define N 3
+#define M 2
+
+double scalar_product(double u[N], double v[N]) {
 	double res = 0;
 
-	for ( int i; i<3; i++) {
+	for ( int i; i<N; i++) {
 		res += u[i]*v[i];
-		printf("res = %f\n", res);
 	}
 
-printf("resres = %f\n", res);
 	return res;
 }
 
-void orthonormalize(double A[3][3]) {
+void orthonormalize(double A[M][N]) {
 	
-	double temp[3];
-	double norm;
+	double temp[N];
+	double norm, temp_norm;
 
-	for(int i = 0; i<3; i++) {
+	for(int i = 0; i<M; i++) {
 		printf("Traitement du vecteur numéro %d\n", i);
-		for (int j = 0; j<3; j++) {
+		for (int j = 0; j<N; j++) {
 			temp[j] = A[i][j];
 		}
 
@@ -33,76 +35,72 @@ void orthonormalize(double A[3][3]) {
 			printf("calcul de la project sur l'axe %d\n", k);
 
 			norm = scalar_product(A[i], A[k]);
-			printf("%f\n", norm);
+			printf("(u,v) = %f\n", norm);
 			norm /= scalar_product(A[k], A[k]);
 
-			printf("%f\n", norm);
+			printf("(u,v) / (u,u) = %f\n", norm);
 
-			for (int j = 0; j<3; j++) {
+			for (int j = 0; j<N; j++) {
 				temp[j] -= norm * A[i][j];
 			}
 
 		}
 
-		for (int j = 0; j<3; j++) {
-			A[i][j] = temp[j];
+		temp_norm = 0;
+		for ( int k = 0; k<N; k++) {
+			temp_norm += temp[k] * temp[k];
+		}
+
+		temp_norm = sqrt(temp_norm);
+
+		for (int j = 0; j<N; j++) {
+			A[i][j] = temp[j] / temp_norm;
 		}
 	}
 }
 
 int main() {
 
-        double A[3][3] = {
+        double A[N][N] = {
                 {1,2,3},
                 {2,5,3},
                 {3,3,15}
         };
 
 	// Initialise S0
-        double q[2][3] = {
-		{4.3,7.87,4.64},
-		{4.3,-7.87,-4.64}
+        double q[M][N] = {
+		{0,3,1},
+		{5,-2,3}
 	};
 
 	// Temp vector
-        double v[2][3];
-        double alpha[2];
+        double v[M][N];
 
 	// A^k*v serie calculation
-        for(int n = 0; n < 20; n++) {
-
-		alpha[0] = 0;
-		alpha[1] = 0;
-
-		// Compute normalization dividende
-		for (int j = 0; j < 2; j++) {
-			for (int i = 0; i<3;i++) {
-				if (alpha[i] < abs(q[i][j]))
-					alpha[i] = q[i][j];
-			}
-                }
+        for(int n = 0; n < 5; n++) {
 
 		// v = A * Q
-		for (int k = 0; k<2; k++) {
-			for (int i = 0; i<3;i++) {
+		for (int k = 0; k<M; k++) {
+			for (int i = 0; i<N;i++) {
 				v[k][i] = 0.0;
 				for (int j = 0; j<3;j++) {
 					v[k][i] += q[k][j]*A[i][j];
 				}
-				v[k][i] = v[k][i] / alpha[k];
+				v[k][i] = v[k][i];
 			}
 		}
 
+		orthonormalize(v);
+
 		// q = v
-                for(int i = 0; i<2; i++) {
-			for(int j = 0; j<3; j++) {
+                for(int i = 0; i<M; i++) {
+			for(int j = 0; j<N; j++) {
 				q[i][j] = v[i][j];
 				printf("[%f]",q[i][j]);
 			}
 			printf("\n");
                 }
                 printf("\n");
-		orthonormalize(A);
 		
         }
 
