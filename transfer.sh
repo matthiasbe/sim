40206feec968582e3828868d6f79a879f00b4114
Training <training@mdlspc151.extra.cea.fr>
2019-02-13 13:49:21 +0100

    Add new matrix 5000x5000 + poincare help script

---
+++ transfer.sh
@@ -0,0 +1,9 @@
+SIM_DIR="../sim3"
+SIM_ID="MIS_BG.699479"
+
+mkdir $SIM_DIR
+scp 	poincare:/gpfs1l/gpfshome/mds/grptraining/training12/sim/$SIM_ID.{err,log}\
+	poincare:/gpfs1l/gpfshome/mds/grptraining/training12/sim/job.sh\
+	poincare:/gpfs1l/gpfshome/mds/grptraining/training12/sim/output.csv\
+	poincare:/gpfs1l/gpfshome/mds/grptraining/training12/sim/.git/refs/heads/master\
+	$SIM_DIR
