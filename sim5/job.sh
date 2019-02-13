f04ce9d3094e6d40f9401f2a9b3d24afa80540e8
Training <training@mdlspc151.extra.cea.fr>
2019-02-13 14:01:29 +0100

    Add simulation 2,4,5 results + new matrix

---
+++ sim5/job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#@ class            = clallmds
+#@ job_name         = MIS_BG
+#@ total_tasks      = 16
+#@ node             = 2
+#@ node_usage       = not_shared
+#@ wall_clock_limit = 00:05:00
+#@ output           = $(job_name).$(jobid).log
+#@ error            = $(job_name).$(jobid).err
+#@ job_type         = mpich
+#@ environment      = COPY_ALL 
+#@ queue
+#
+module load intel openmpi
+module load mkl/11.0
+mpirun ./build/bench -i 50 -e 5 -m 10 -f test/matrices/gemat12.mtx
