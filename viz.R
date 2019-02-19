e9bb67f5367caccba94c2d845b8f58efe0c451fc
Pierre Granger <pierre.granger@ens-cachan.fr>
2019-02-19 20:26:28 +0100

    Fin partie seq

--- viz.R
+++ viz.R
@@ -1,20 +1,20 @@
 library("dplyr")
 library("ggplot2")
 
-# data = read.csv("/home/pgranger/Documents/CHPS/MPNA/sim_wolock/sim/build/output.csv") %>%
-#   as_tibble() %>%
-#   mutate(eigindex = as.factor(as.character(eigindex)))
+data = read.csv("/home/pgranger/Documents/CHPS/MPNA/sim/build/output.csv") %>%
+  as_tibble() %>%
+  mutate(eigindex = as.factor(as.character(eigindex)))
+
+data %>%
+  filter(iteration > 0) %>%
+  filter(type == "t") %>%
+  ggplot(aes(x = iteration, y = value)) + geom_path() + geom_point() + ylab("temps") + geom_smooth()
+
+# omp = read.csv("/home/pgranger/Documents/CHPS/MPNA/sim/tvsM.csv") %>%
+#   as_tibble()
 # 
-# data %>%
-#   filter(iteration > 0) %>%
-#   filter(type == "t") %>%
-#   ggplot(aes(x = iteration, y = value)) + geom_path() + geom_point() + ylab("temps") + geom_smooth()
-
-omp = read.csv("/home/pgranger/Documents/CHPS/MPNA/sim/tvsM.csv") %>%
-  as_tibble()
-
-omp %>%
-  ggplot(aes(x = M, y = t)) + geom_path() + geom_point() + ylab("temps (s)") + xlab("M taille de la matrice") + scale_y_log10() + scale_x_log10() +stat_smooth(method = "lm")
+# omp %>%
+#   ggplot(aes(x = M, y = t)) + geom_path() + geom_point() + ylab("temps (s)") + xlab("M taille de la matrice") + scale_y_log10() + scale_x_log10() +stat_smooth(method = "lm")
 
 # data_wlock = read.csv("/home/pgranger/Documents/CHPS/MPNA/sim/measures.csv") %>%
 #   as_tibble()
